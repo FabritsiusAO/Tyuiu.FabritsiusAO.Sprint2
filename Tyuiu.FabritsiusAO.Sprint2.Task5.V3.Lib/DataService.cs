fde2e80fbe5739591cf62be0e3b9507d7b090584
Андрей Фабрициус <Tyuiu.FabritsiusAO@outlook.com>
2024-10-14 12:36:13 +0500

    Task5

---
+++ Tyuiu.FabritsiusAO.Sprint2.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.FabritsiusAO.Sprint2.Task5.V3.Lib
+{
+    public class DataService : ISprint2Task5V3
+    {
+        public string FindDayName(int value)
+        {
+            string res;
+            switch (value)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 7:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    res = "Порядковый номер должен быть в промежутке от 1 до 7. Значение: " + value;
+                    break;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FabritsiusAO.Sprint2.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.FabritsiusAO.Sprint2.Task5.V3.Lib;
+
+namespace Tyuiu.FabritsiusAO.Sprint2.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDayName()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual("Понедельник",ds.FindDayName(1));
+            Assert.AreEqual("Вторник", ds.FindDayName(2));
+            Assert.AreEqual("Среда", ds.FindDayName(3));
+            Assert.AreEqual("Четверг", ds.FindDayName(4));
+            Assert.AreEqual("Пятница", ds.FindDayName(5));
+            Assert.AreEqual("Суббота", ds.FindDayName(6));
+            Assert.AreEqual("Воскресенье", ds.FindDayName(7));
+            Assert.AreEqual("Порядковый номер должен быть в промежутке от 1 до 7. Значение: 8", ds.FindDayName(8));
+        }
+    }
+}
